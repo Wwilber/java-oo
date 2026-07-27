0ca358b5207783e72e1116c45191ef736e82348a
wilber ribeiro <wilber.programacao@gmail.com>
2026-07-27 20:29:55 -0300

    aula_520_atribuindo o objeto da composição.

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,42 @@
 public class Principal {
     public static void main(String[] args) {
         Carro meuCarro = new Carro();
+        Pessoa eu = new Pessoa();
+        eu.nome = "wilber Ribeiro";
+        eu.cpf = "371.778.693-87";
+        eu.anoNascimento= 1972;
+
 
         meuCarro.modelo = "HR-V";
         meuCarro.anoFabricacao = 2021;
         meuCarro.cor = "Vermelho";
         meuCarro.fabricante = "Honda";
 
-        Carro seuCarro = new Carro();
+//        meuCarro.proprietario = new Pessoa();
+//        meuCarro.proprietario.nome = "Wilber Ribeiro";
+//        meuCarro.proprietario.cpf = "371.778.693-87";
+//        meuCarro.proprietario.anoNascimento = 1972;
 
-        seuCarro.modelo = "Golf";
-        seuCarro.anoFabricacao = 2011;
-        seuCarro.cor = "Branco";
-        seuCarro.fabricante = "volks";
+        meuCarro.proprietario=eu;
+
+
+//        Carro seuCarro = new Carro();
+//
+//        seuCarro.modelo = "Golf";
+//        seuCarro.anoFabricacao = 2011;
+//        seuCarro.cor = "Branco";
+//        seuCarro.fabricante = "volks";
 
         System.out.printf("Modelo: %s%n", meuCarro.modelo);
         System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+        System.out.printf("Proprietario: %s%n", meuCarro.proprietario.nome);
+        System.out.printf("Proprietario: %s%n", meuCarro.proprietario.cpf);
+        System.out.printf("Proprietario: %s%n", meuCarro.proprietario.anoNascimento);
 
-        System.out.println("-------");
-        System.out.printf("Modelo: %s%n", seuCarro.modelo);
-        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
+
+//        System.out.println("-------");
+//        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+//        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
 
 
     }
